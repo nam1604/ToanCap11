53d37bcc0017c1f7b619620a70c1e2b6bb6f1abf
quynam <quynamle123@gmail.com>
2020-10-22 16:48:55 +0700

    version 1.0.0

--- toancap11/Source.cpp
+++ toancap11/Source.cpp
@@ -3,7 +3,23 @@ using namespace std;
 
 int main() {
 	
-	cout << "Hello word!!";
-	cout << "Hello word!!adasdaafasgasga";
+	cout << "\nHello word!!";
+	cout << "\nHello word!!adasdaafasgasga";
 	//1.0.0
+	int a, b;
+	cout << "\nNhap vao lan luot 2 so a , b :";
+	cin >> a;
+	cin >> b;
+	
+	int tong,hieu;
+	float nhan,chia;
+
+	tong = a + b;
+	hieu = a - b;
+	nhan = a * b;
+	chia = float(a / b);
+
+	cout << "Tong :" << tong << ",hieu" << hieu << ",nhan" << nhan << ",chia" << chia;
+
+	
 }
